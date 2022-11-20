d4acbdde8c192c9e24a93e536ae7456741b10474
KuonRuri <kuonruri@gmail.com>
2022-11-20 10:36:47 +0900

    add keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,6 +63,17 @@ opt.shiftwidth = 4
 -- キーマップ
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('i', 'っｊ', '<Esc>')
+vim.keymap.set('n', '<Down>', '<C-w>j')
+vim.keymap.set('n', '<Up>', '<C-w>k')
+vim.keymap.set('n', '<Left>', '<C-w>h')
+vim.keymap.set('n', '<Right>', '<C-w>l')
+vim.keymap.set('v', '<', '<gv');
+vim.keymap.set('v', '>', '>gv');
+vim.keymap.set('n', 'p', ']p');
+vim.keymap.set('n', 'P', ']P');
+vim.keymap.set('n', 'Y', 'y$');
+vim.keymap.set('n', '+', '<C-a>')
+vim.keymap.set('n', '-', '<C-x>')
 
 -- カラースキーマを設定
 vim.o.background = "dark"
