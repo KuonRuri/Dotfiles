f972e43b9853f83d8b9ddd1275d543408b9c74bf
kuonruri <kuonruri@gmail.com>
2023-01-12 21:53:46 +0900

    REMOVE
    remove dein load setting in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,20 +9,20 @@ if not string.match(vim.o.runtimepath, '/dein.vim') then
 end
 
 -- 自動で dein#recache_runtimepath() する
-vim.api.nvim_set_var('dein#auto_recache', 1)
+--vim.api.nvim_set_var('dein#auto_recache', 1)
 -- neovimのリモートプラグインを遅延読み込み
-vim.api.nvim_set_var('dein#lazy_rplugins', 1)
-vim.api.nvim_set_var('dein#enable_notification', 1)
-vim.api.nvim_set_var('dein#install_max_processes', 16)
-vim.api.nvim_set_var('dein#install_message_type', 'none')
-vim.api.nvim_set_var('dein#enable_notification', 1)
+--vim.api.nvim_set_var('dein#lazy_rplugins', 1)
+--vim.api.nvim_set_var('dein#enable_notification', 1)
+--vim.api.nvim_set_var('dein#install_max_processes', 16)
+--vim.api.nvim_set_var('dein#install_message_type', 'none')
+--vim.api.nvim_set_var('dein#enable_notification', 1)
 
 if vim.call('dein#load_state', dein_dir) == 1 then
 	local dein_toml_dir = vim.env.HOME .. '/.config/nvim'
 	local dein_toml = dein_toml_dir .. '/dein.toml'
 	local dein_toml_lazy = dein_toml_dir .. '/dein_lazy.toml'
 
-	vim.call('dein#begin', dein_dir, {vim.fn.expand('<sfile>'), dein_toml, dein_toml_lazy, dein_toml_input})
+	vim.call('dein#begin', dein_dir, {vim.fn.expand('<sfile>'), dein_toml, dein_toml_lazy})
 
 	vim.call('dein#load_toml', dein_toml, {lazy = 0})
 	vim.call('dein#load_toml', dein_toml_lazy, {lazy = 1})
