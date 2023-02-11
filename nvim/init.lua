3a22d0b280c25b85657188085b36c71f4efe5b8d
kuonruri <kuonruri@gmail.com>
2023-02-11 19:42:45 +0900

    ADD
    Add keymap. Push <Esc><Esc> to remove hilight.

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,13 +73,14 @@ vim.keymap.set('n', '<Down>', '<C-w>j')
 vim.keymap.set('n', '<Up>', '<C-w>k')
 vim.keymap.set('n', '<Left>', '<C-w>h')
 vim.keymap.set('n', '<Right>', '<C-w>l')
-vim.keymap.set('v', '<', '<gv');
-vim.keymap.set('v', '>', '>gv');
-vim.keymap.set('n', 'p', ']p');
-vim.keymap.set('n', 'P', ']P');
-vim.keymap.set('n', 'Y', 'y$');
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('n', 'p', ']p')
+vim.keymap.set('n', 'P', ']P')
+vim.keymap.set('n', 'Y', 'y$')
 vim.keymap.set('n', '+', '<C-a>')
 vim.keymap.set('n', '-', '<C-x>')
+vim.keymap.set('n', '<Esc><Esc>', ':noh<CR>')
 
 -- setup lsp
 local mason = require('mason')
