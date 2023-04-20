d53300f490dcad6b5e395346b086d8a48ce3437e
kuonruri <kuonruri@gmail.com>
2023-04-20 23:38:23 +0900

    CHANGE
    Change a few Neovim config files.

--- nvim/lua/Options.lua
+++ nvim/lua/Options.lua
@@ -1,31 +1,19 @@
 local opt = vim.opt
 
--- Encording
--- opt.encoding = 'utf-8'
--- opt.scriptencoding = 'utf-8'
-
--- Use 24-bit colors
+opt.encoding = "utf-8"
 opt.termguicolors = true
-
--- Show indent
 opt.list = true
 opt.listchars = {tab = '>>-', trail = '-', nbsp = '+'}
-
--- Show number
 opt.number = true
 opt.relativenumber = true;
-
--- Change indent width
 opt.autoindent = true
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.shiftwidth = 4
-
--- Disable swap file
 opt.swapfile = false
-
--- Disable mouse
 opt.mouse = ""
-
--- Float transparent
 opt.winblend = 5
+opt.hlsearch = false
+opt.shell = "zsh"
+opt.splitbelow = true
+opt.splitright = true
