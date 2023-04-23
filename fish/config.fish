850d4bd7e344f56618ce5ad4329c5d3ebf01e8e4
kuonruri <kuonruri@gmail.com>
2023-04-23 20:20:28 +0900

    FIX
    Fix aliase.

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ end
 abbr ls 'ls -al'
 abbr gaa 'git add .'
 abbr gc 'git commit'
-abbr gp 'git rev-parse --abbrev-ref HEAD | xrandr git push origin'
+abbr gp 'git rev-parse --abbrev-ref HEAD | xargs git push origin'
 abbr gpm 'git push origin master'
 abbr ga 'git add . && git commit'
 abbr gs 'git status'
