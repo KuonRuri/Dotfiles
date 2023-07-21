5da85464ede69ff8124fee243a8e8a5d6fb19f40
kuonruri <kuonruri@gmail.com>
2023-07-21 04:19:38 +0000

    FIX

--- nvim/lua/init/Plugin.lua
+++ nvim/lua/init/Plugin.lua
@@ -1,42 +1,35 @@
--- 自動で dein#recache_runtimepath() する
---vim.api.nvim_set_var('dein#auto_recache', 1)
--- neovimのリモートプラグインを遅延読み込み
---vim.api.nvim_set_var('dein#lazy_rplugins', 1)
---vim.api.nvim_set_var('dein#enable_notification', 1)
---vim.api.nvim_set_var('dein#install_max_processes', 16)
---vim.api.nvim_set_var('dein#install_message_type', 'none')
+local api = vim.api
 
-local dein_dir = vim.env.HOME .. '/.cache/dein'
-local dein_repo_dir = dein_dir .. '/repos/github.com/Shougo/dein.vim'
+local dein_dir = vim.fn.expand('~/.cache/dein')
+local dein_repo_dir = dein_dir..'/repos/github.com/Shougo/dein.vim'
 
-if not string.match(vim.o.runtimepath, '/dein.vim') then
-	if vim.fn.isdirectory(dein_repo_dir) ~= 1 then
-		os.execute('git clone https://github.com/Shougo/dein.vim '..dein_repo_dir)
-	end
-	vim.o.runtimepath = dein_repo_dir .. ',' .. vim.o.runtimepath
+vim.o.runtimepath = dein_repo_dir..','..vim.o.runtimepath
+
+-- dein install check
+if (vim.fn.isdirectory(dein_repo_dir) == 0) then
+  os.execute('git clone https://github.com/Shougo/dein.vim '..dein_repo_dir)
 end
 
-if vim.call('dein#load_state', dein_dir) == 1 then
-	local dein_toml_dir = vim.env.HOME .. '/.config/nvim/Plugins'
-	local dein_toml = dein_toml_dir .. '/dein.toml'
-	local dein_toml_lazy = dein_toml_dir .. '/dein_lazy.toml'
-
-	vim.call('dein#begin', dein_dir, {vim.fn.expand('<sfile>'), dein_toml, dein_toml_lazy})
-
-	vim.call('dein#load_toml', dein_toml, {lazy = 0})
-	vim.call('dein#load_toml', dein_toml_lazy, {lazy = 1})
-	vim.call('dein#end')
-	vim.call('dein#save_state')
+-- begin settings
+if (vim.fn['dein#load_state'](dein_dir) == 1) then
+  local rc_dir = vim.fn.expand('~/.config/nvim')
+  local toml_nomal = rc_dir..'/Plugins/dein.toml'
+  local toml_lazy = rc_dir..'/Plugins/dein_lazy.toml'
+  vim.fn['dein#begin'](dein_dir)
+  vim.fn['dein#load_toml'](toml_nomal, { lazy = 0 })
+  vim.fn['dein#load_toml'](toml_lazy, { lazy = 1 })
+  vim.fn['dein#end']()
+  vim.fn['dein#save_state']()
 end
 
--- Plugin install check
+-- plugin install check
 if (vim.fn['dein#check_install']() ~= 0) then
-	vim.fn['dein#install']()
+  vim.fn['dein#install']()
 end
 
--- Plugin remove check
+-- plugin remove check
 local removed_plugins = vim.fn['dein#check_clean']()
 if vim.fn.len(removed_plugins) > 0 then
-	vim.fn.map(removed_plugins, "delete(v:val, 'rf')")
-	vim.fn['dein#recache_runtimepath']()
+  vim.fn.map(removed_plugins, "delete(v:val, 'rf')")
+  vim.fn['dein#recache_runtimepath']()
 end
